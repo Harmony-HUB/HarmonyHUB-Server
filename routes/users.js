79b79b7b2c95c73b9fc88052521e60f866ab0bfc
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-04-08 14:31:19 +0900

    first commit

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.send("respond with a resource");
+});
+
+module.exports = router;
